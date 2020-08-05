0c9c8076429034066062c94eeda310e17c2beafb
Leonardo Buck de Godoy <leonardo@timais.com>
2020-08-05 17:26:15 -0300

    .

---
+++ src/backend/index.js
@@ -0,0 +1,12 @@
+import { ipcMain } from 'electron'
+
+import pathsToRows from './pathsToRow'
+import prepareData from './prepareData'
+import groupWords from './groupWords'
+
+ipcMain.on('process-subtitles', (event, paths) => {
+    pathsToRows(paths)
+        .then(rows => prepareData(rows))
+        .then(words => groupWords(words))
+        .then(groupedWords => event.reply('process-subtitles', groupedWords))    
+})
